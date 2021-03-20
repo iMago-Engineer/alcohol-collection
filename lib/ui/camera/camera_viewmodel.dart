e084b808ba3f2ac06b4082c566852d4fe371aeeb
Kaze Shindo <kaze.bknb@outlook.jp>
2021-03-20 14:24:58 +0900

    send image to Firebase

--- lib/ui/camera/camera_viewmodel.dart
+++ lib/ui/camera/camera_viewmodel.dart
@@ -1,14 +1,25 @@
+import 'dart:io';
 import 'package:stacked/stacked.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:alcohol_collection/service_locator.dart';
+import 'package:alcohol_collection/services/firebase_api.dart';
 
 class CameraViewModel extends BaseViewModel {
+  final _firebase_api = servicesLocator<FirebaseAPIService>();
   final picker = ImagePicker();
 
+  //  画像取得
   Future getImageFromGalleryOrCamera(source) async {
     // 画像が重すぎるため圧縮
     return await picker.getImage(source: source, imageQuality: 30);
   }
 
+  // Firebase に送る処理
+  Future uploadImageToFirebase(path, file) async {
+    return await _firebase_api.uploadImage(path, File(file.path));
+  }
+
+  // 画像選択 & Firebase 送信
   Future imageFunction(String type) async {
     setBusy(true);
 
@@ -19,14 +30,13 @@ class CameraViewModel extends BaseViewModel {
       file = await getImageFromGalleryOrCamera(ImageSource.camera);
     }
 
+    final imageUrl = await uploadImageToFirebase("path/", file);
+    if (imageUrl != null) {
+      // pass
+    } else {
+      // pass
+    }
+
     setBusy(false);
-    // final imageUrl = await uploadImageToFirebase(file);
-    // if (imageUrl != null) {
-    //   await _api.createImage(wish.id, imageUrl);
-    //   wish.imageUrl = imageUrl;
-    // } else {
-    //   _navigator.pushNamedAndRemoveUntil(routeName: '/error');
-    // }
-    // _navigator.pushNamedAndRemoveUntil(routeName: '/root');
   }
 }
