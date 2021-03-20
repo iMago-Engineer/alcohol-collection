d7161e16278d33372fdd640f7b0b5f8fb1f30743
sssht03 <matsuo.shuta@arakawa-lab.com>
2021-03-20 13:03:22 +0900

    add files

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,8 +8,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:alcohol_correction/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
@@ -28,3 +26,5 @@ void main() {
     expect(find.text('1'), findsOneWidget);
   });
 }
+
+class MyApp {}
