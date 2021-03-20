78e41ceae1ad194b2161b41e9f6453edc5a2d6b4
KY-Ng <raionkurane@gmail.com>
2021-03-20 16:07:22 +0900

    update factory to receive QueryDocumentSnapshot instead of Map

--- lib/models/comment.dart
+++ lib/models/comment.dart
@@ -1,25 +1,29 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Comment {
+  String docId;
   DateTime postedAt;
   String content;
 
-  Comment({this.postedAt, this.content});
+  Comment({this.docId, this.postedAt, this.content});
 
-  factory Comment.fromFirestore(Map<String, dynamic> docData) {
+  factory Comment.fromFirestore(QueryDocumentSnapshot doc) {
+    final docData = doc.data();
+
     return Comment(
+      docId: doc.id,
       postedAt: (docData['postedAt'] as Timestamp).toDate(),
       content: docData['content'] as String,
     );
   }
 
   @override
-  String toString() => '{ Comment; postedAt: $postedAt, content: $content }';
+  String toString() =>
+      '{ Comment; docId: $docId, postedAt: $postedAt, content: $content }';
 }
 
-List<Comment> parseIntoComments(List<dynamic> firestoreDocList) {
+List<Comment> parseIntoComments(List<QueryDocumentSnapshot> firestoreDocList) {
   return firestoreDocList
-      .map((firestoreDoc) =>
-          Comment.fromFirestore(firestoreDoc as Map<String, dynamic>))
+      .map((firestoreDoc) => Comment.fromFirestore(firestoreDoc))
       .toList();
 }
